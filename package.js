5c0b0d5f1e27b95240012d4a3ce0e8eff92886fe
nodexperts <development@nodexperts.com>
2017-07-27 11:31:09 +0530

    Adding travis ci support for the package

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'menu-generator',
+  name: 'nodexpert:menu-generator',
   version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Generate Customisable Menu',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/nodexpertsdev/menu-generator.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,14 +12,14 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.5');
-  api.use(['ecmascript','blaze','templating']);
+  api.use(['ecmascript','blaze@2.3.2','templating@1.3.2']);
   api.addFiles(['menu-generator.html','menu-generator.js'], 'client');
   api.mainModule('listBuilder.js');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
-  api.use(['practicalmeteor:chai','jquery','blaze','templating']);
+  api.use(['practicalmeteor:chai@2.1.0','jquery@1.11.10','blaze@2.3.2','templating@1.3.2']);
   api.addFiles(['menu-generator.html','menu-generator.js'], 'client');
   api.mainModule('menu-generator-tests.js', 'client');
 });
